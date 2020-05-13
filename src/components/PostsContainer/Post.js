5ed587e6f4f31d9c1ac083fe438c21c5ac95ff50
JDMTias <matiasiturbide@gmail.com>
2020-05-13 14:41:04 -0600

    fixed a bug...relized css is not done...

--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,6 +1,5 @@
 // You will add code in this file
 import React, {useState} from "react";
-import {data, setData} from '../../App'
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
